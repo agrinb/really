8810e62d9070410640d5571d0e860ada270d0508
Alek <agrinb@gmail.com>
2014-07-03 15:36:44 -0400

    Roles are being stored when user signs up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
     binding.pry
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :first_name, :last_name, :role, :cell_phone) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name, :role, :cell_phone) }
   end
 end
