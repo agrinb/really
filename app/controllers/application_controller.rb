fb3fdf207e5410ecd8f2fca40f5602dd2cd3769c
Alek <agrinb@gmail.com>
2014-07-03 13:56:46 -0400

    Added User model, devise views, strong params for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :first_name, :last_name, :role, :cell_phone) }
+  end
 end
