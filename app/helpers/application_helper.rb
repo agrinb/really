90834d7554ccf9660edaf8ae5d0ca6f73c053ad4
Alek <agrinb@gmail.com>
2014-07-04 13:00:19 -0400

    flash message bootstrapified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_class(key)
+    binding.pry
+    if "success"
+      "alert alert-success"
+    elsif "notice"
+      "alert alert-info"
+    else
+      "alert alert-error"
+    end
+  end
 end
