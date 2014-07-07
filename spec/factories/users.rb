91a210cbec423a56d3b548358b1ab4e1d4067be8
Alek <agrinb@gmail.com>
2014-07-07 14:56:04 -0400

    added users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email) { |n| "email#{n}@gmail.com" }
+    password "supersecret"
+    first_name "Mike"
+    last_name "Jones"
+    role "seller"
+    sequence(:cell_phone) { |n| "#{n}23-456-7890" }
   end
 end
