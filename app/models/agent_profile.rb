6c45921c95318ddcb25fb0d19309bc4ffbe1c80a
Alek <agrinb@gmail.com>
2014-07-08 15:12:44 -0400

    fixed nested Agent profile form with Eric's help

--- app/models/agent_profile.rb
+++ app/models/agent_profile.rb
@@ -1,9 +1,9 @@
 class AgentProfile < ActiveRecord::Base
-  validates :user_id, presence: true, uniqueness: true
+  validates :user, presence: true, uniqueness: true
   validates :zip_code, presence: true
   validates :from_hour, presence: true
   validates :to_hour, presence: true
   validates :description, presence: true
   validates :years_of_experience, presence: true
-  has_one :user
+  belongs_to :user
 end
