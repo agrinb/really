5073b81b9b8d7c1e48259e49d4471e1e0a7883f3
Alek <agrinb@gmail.com>
2014-07-08 20:09:28 -0400

    added index action

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-  before_action :set_user, only: [:new, :create]
+  before_action :set_user, only: [:new, :create, :index ]
 
   def new
     @user = User.find(params[:user_id])
@@ -7,17 +7,18 @@ class PropertiesController < ApplicationController
   end
 
   def index
-    "hello world"
+    @properties = @user.properties.all
   end
 
   def create
     @property = Property.new(property_params)
     @property.user_id = set_user.id
-        binding.pry
     if @property.save
       flash[:notice] = "Congratulations! You've just listed your property!"
+      redirect_to user_properties_path
     else
       flash[:alert] = "Your property could not be saved!"
+      render :new
     end
   end
 
