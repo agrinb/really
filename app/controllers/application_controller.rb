f54e7ab2e24a0456fb2e61e7904cb3d78eabec7c
Alek <agrinb@gmail.com>
2014-07-22 21:39:50 -0400

    updated welcome controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+
   def after_sign_in_path_for(resource)
     if current_user.role == "agent"
     "/agent_profiles/#{current_user.agent_profile.id}/appointments"
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController <ApplicationController
 
-def index
 
+def index
+  if current_user
+    redirect_to after_sign_in_path_for(current_user)
+  end
 end
 
 
