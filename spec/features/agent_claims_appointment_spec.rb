10624ec2dda279b0214cb5d0e7516a7978ed6b00
Alek <agrinb@gmail.com>
2014-08-04 19:48:22 -0400

    Passing agent claims appt test

--- spec/features/agent_claims_appointment_spec.rb
+++ spec/features/agent_claims_appointment_spec.rb
@@ -12,32 +12,21 @@ feature 'agent signs in claim appointments', %Q{
       sign_in_as(user)
       property = FactoryGirl.create(:property, user: user, zip_code: '02111')
       appointment1 = FactoryGirl.create(:appointment, property: property)
-      # appointment2 = FactoryGirl.create(:appointment, property: property)
-      # appointment3 = FactoryGirl.create(:appointment, property: property)
+      appointment2 = FactoryGirl.create(:appointment, property: property)
       click_on 'Sign Out'
 
 
-
-
-    #in_browser(:two) do
       visit new_user_session_path
       agent_user = FactoryGirl.create(:user, role: 'agent')
       agent = FactoryGirl.create(:agent_profile, user: agent_user, zip_code: '02111')
-      #user = agent.user
       sign_in_as(agent_user)
-      click_on 'Claim'
-      # click_on 'Claim'
-      # click_on 'Claim'
+      first('.panel-default').click_link('Claim')
 
 
       within ".panel-success" do
-      expect(page).to have_content property.address
-      expect(page).to have_content appointment1.visitor_phone
-      expect(page).to have_content appointment1.visitor
+        expect(page).to have_content property.address
+        expect(page).to have_content appointment1.visitor_phone
+        expect(page).to have_content appointment1.visitor
       end
-      # expect(page).to have_content appointment2.visitor_phone
-      # expect(page).to have_content appointment2.visitor
-      # expect(page).to have_content appointment3.visitor_phone
-      # expect(page).to have_content appointment3.visitor
   end
 end
