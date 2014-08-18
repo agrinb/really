4e5946ada90783caf1327e0191b0fbc0440716d6
agrinb <agrinb@gmail.com>
2014-08-18 19:33:47 -0400

    added to spec helper travis-ci helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,19 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
+
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+  module Kernel
+    alias :__at_exit :at_exit
+    def at_exit(&block)
+      __at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        block.call
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
 # FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
 # FactoryGirl.find_definitions
 
